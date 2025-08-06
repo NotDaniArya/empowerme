13f3ef4a1c6057aca52ebcc9b18d43b0d4907be5
Muhammad Dani Arya Putra <13120210004@umi.ac.id>
2025-08-06 22:37:16 +0800

    refactor: berita terbaru di home sudah di perbaiki

--- lib/user_features/home/presentation/screens/widgets/information_slider.dart
+++ lib/user_features/home/presentation/screens/widgets/information_slider.dart
@@ -1,19 +1,22 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:new_empowerme/user_features/edukasi/presentation/berita/providers/berita_provider.dart';
 import 'package:new_empowerme/utils/constant/images.dart';
 import 'package:new_empowerme/utils/constant/sizes.dart';
 
 import '../../../../../utils/constant/colors.dart';
+import '../../../../edukasi/presentation/berita/detail_berita_screen.dart';
 
-class InformationSlider extends StatefulWidget {
+class InformationSlider extends ConsumerStatefulWidget {
   const InformationSlider({super.key});
 
   @override
-  State<InformationSlider> createState() => _InformationSlider();
+  ConsumerState<InformationSlider> createState() => _InformationSlider();
 }
 
-class _InformationSlider extends State<InformationSlider> {
+class _InformationSlider extends ConsumerState<InformationSlider> {
   int _currentIndex = 0;
   final _controller = CarouselSliderController();
   final _informationHomeScreenImage = MyImages.informationHomeScreen;
@@ -21,90 +24,142 @@ class _InformationSlider extends State<InformationSlider> {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final beritaState = ref.watch(beritaViewModel);
+
+    return _buildBody(context, beritaState);
+  }
+
+  Widget _buildBody(BuildContext context, BeritaState state) {
+    final textTheme = Theme.of(context).textTheme;
+
+    if (state.isLoading) {
+      return const SizedBox(
+        height: 160,
+        child: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    if (state.error != null) {
+      return Center(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.scaffoldPadding),
+          child: Text(
+            'Terjadi kesalahan: ${state.error}',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      );
+    }
+
+    if (state.berita == null || state.berita!.isEmpty) {
+      return const Center(child: Text('Tidak ada berita yang ditemukan.'));
+    }
+
+    final limitedBeritaList = state.berita!.take(3).toList();
+
+    final currentBerita = limitedBeritaList.isNotEmpty
+        ? state.berita![_currentIndex]
+        : null;
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            // Carousel Slider
-            CarouselSlider(
-              items: _informationHomeScreenImage
-                  .map(
-                    (banner) => ClipRRect(
-                      borderRadius: BorderRadiusGeometry.circular(8),
-                      child: CachedNetworkImage(
-                        // height: 200,
-                        imageUrl: banner,
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) => Center(
-                              child: CircularProgressIndicator(
-                                value: downloadProgress.progress,
+        InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    DetailBeritaScreen(berita: currentBerita!),
+              ),
+            );
+          },
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              // Carousel Slider
+              CarouselSlider(
+                items: limitedBeritaList
+                    .map(
+                      (beritaItem) => ClipRRect(
+                        borderRadius: BorderRadiusGeometry.circular(8),
+                        child: CachedNetworkImage(
+                          // height: 200,
+                          imageUrl: beritaItem.displayImageUrl,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          progressIndicatorBuilder:
+                              (context, url, downloadProgress) => Center(
+                                child: CircularProgressIndicator(
+                                  value: downloadProgress.progress,
+                                ),
                               ),
-                            ),
-                        errorWidget: (context, url, error) =>
-                            const Icon(Icons.error),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                carouselController: _controller,
+                options: CarouselOptions(
+                  viewportFraction: 1.0,
+                  aspectRatio: 16 / 9,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 5),
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
+                ),
+              ),
+
+              if (currentBerita != null)
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsetsGeometry.symmetric(
+                      vertical: 6,
+                      horizontal: 12,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.black45,
+                      borderRadius: BorderRadiusGeometry.only(
+                        bottomRight: Radius.circular(8),
+                        bottomLeft: Radius.circular(8),
                       ),
                     ),
-                  )
-                  .toList(),
-              carouselController: _controller,
-              options: CarouselOptions(
-                viewportFraction: 1.0,
-                aspectRatio: 16 / 9,
-                autoPlay: true,
-                autoPlayInterval: const Duration(seconds: 5),
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-              ),
-            ),
-
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                padding: const EdgeInsetsGeometry.symmetric(
-                  vertical: 6,
-                  horizontal: 12,
-                ),
-                decoration: const BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadiusGeometry.only(
-                    bottomRight: Radius.circular(8),
-                    bottomLeft: Radius.circular(8),
+                    child: Column(
+                      children: [
+                        Text(
+                          currentBerita.title,
+                          textAlign: TextAlign.center,
+                          style: textTheme.bodySmall!.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: TSizes.smallSpace / 2),
+                        Text(
+                          currentBerita.author ?? 'Sumber Tidak Diketahui',
+                          textAlign: TextAlign.center,
+                          style: textTheme.bodySmall!.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      'IDAI: Anak Sehat dan Cerdas Jadi Kunci Menuju Indonesia Emas 2045',
-                      textAlign: TextAlign.center,
-                      style: textTheme.bodySmall!.copyWith(color: Colors.white),
-                    ),
-                    const SizedBox(height: TSizes.smallSpace / 2),
-                    Text(
-                      'Detik.com',
-                      textAlign: TextAlign.center,
-                      style: textTheme.bodySmall!.copyWith(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
         const SizedBox(height: TSizes.smallSpace),
         // Indikator Titik (Dots)
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: _informationHomeScreenImage.asMap().entries.map((entry) {
+          children: limitedBeritaList.asMap().entries.map((entry) {
             final index = entry.key;
             return GestureDetector(
               onTap: () => _controller.animateToPage(index),
