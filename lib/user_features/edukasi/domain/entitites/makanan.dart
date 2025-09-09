51de31f6841eeab88ff0cad5173f71a92f39136d
Muhammad Dani Arya Putra <13120210004@umi.ac.id>
2025-09-09 20:43:15 +0800

    feat: edukasi makanan sudah ditambah, hanya gambar yang tidak muncul

---
+++ lib/user_features/edukasi/domain/entitites/makanan.dart
@@ -0,0 +1,38 @@
+import 'package:equatable/equatable.dart';
+
+class Makanan extends Equatable {
+  final String link;
+  final String title;
+  final String source;
+  final String date;
+  final String description;
+  final String thumbnail;
+
+  const Makanan({
+    required this.link,
+    required this.title,
+    required this.source,
+    required this.date,
+    required this.description,
+    required this.thumbnail,
+  });
+
+  String get displayImageUrl {
+    const String placeholder =
+        'https://mobeng.id/wp-content/uploads/2021/10/placeholder-1-1.png';
+    if (thumbnail.isNotEmpty) {
+      return thumbnail;
+    }
+    return placeholder;
+  }
+
+  @override
+  List<Object?> get props => [
+    link,
+    title,
+    source,
+    date,
+    description,
+    thumbnail,
+  ];
+}
